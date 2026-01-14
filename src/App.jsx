17f702174829a7a403cd4650c26d055e62961342
PoongzaGoon <fndltm021213@naver.com>
2026-01-14 10:42:13 +0900

    Fade-in out

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Input from './pages/Input';
@@ -6,15 +7,40 @@ import Result from './pages/Result';
 import Ranking from './pages/Ranking';
 
 function App() {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransitionStage] = useState('fade-in');
+
+  useEffect(() => {
+    if (
+      location.pathname !== displayLocation.pathname ||
+      location.search !== displayLocation.search ||
+      location.hash !== displayLocation.hash
+    ) {
+      setTransitionStage('fade-out');
+    }
+  }, [location, displayLocation]);
+
+  const handleAnimationEnd = () => {
+    if (transitionStage === 'fade-out') {
+      setDisplayLocation(location);
+      setTransitionStage('fade-in');
+    }
+  };
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/input" element={<Input />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/ranking" element={<Ranking />} />
-      </Routes>
-    </Layout>
+    <div
+      className={`page-transition ${transitionStage}`}
+      onAnimationEnd={handleAnimationEnd}
+    >
+      <Layout>
+        <Routes location={displayLocation}>
+          <Route path="/" element={<Home />} />
+          <Route path="/input" element={<Input />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/ranking" element={<Ranking />} />
+        </Routes>
+      </Layout>
+    </div>
   );
 }
 
